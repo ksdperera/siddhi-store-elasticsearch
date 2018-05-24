84806e82fc9346723e0470d9689d03ccecf6db28
sajithd <sajithd@wso2.com>
2018-05-24 14:24:54 +0530

    Add Siddhi Elasticsearch store extension source

---
+++ component/src/test/java/org/wso2/extension/siddhi/store/elasticsearch/TestCaseOfElasticsearchEventTable.java
@@ -0,0 +1,6 @@
+package org.wso2.extension.siddhi.store.elasticsearch;
+
+public class TestCaseOfElasticsearchEventTable {
+        // If you will know about this related testcase,
+        // refer https://github.com/wso2-extensions/siddhi-store-rdbms/tree/master/component/src/test
+}
